d9a59876a20c4cce46bf1381e720cc6badaf3976
Michael Brenan <blacksmithgu@gmail.com>
2020-05-05 20:00:11 -0500

    Fix build

--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,22 @@
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import typescript from 'rollup-plugin-typescript2';
+import copy from 'rollup-plugin-copy';
 import { terser } from 'rollup-plugin-terser';
 
 export default {
     input: 'src/main.ts',
     output: { dir: 'dist', format: 'umd' },
     watch: { include: 'src/**' },
-    plugins: [typescript(), commonjs(), resolve(), terser()]
+    plugins: [
+        typescript(),
+        commonjs(),
+        resolve(),
+        terser(),
+        copy({
+            targets: [
+                { src: "src/*.html", dest: "dist/" },
+                { src: "src/*.css", dest: "dist/" }
+            ]
+        })]
 };
